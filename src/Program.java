2015ebcb5d23b9645ecebe93cc47e5dca7dd830d
Timothy Liu <liuxf19@163.com>
2023-06-10 02:30:07 +0800

    test: :children_crossing: add `--test` command to launch console test

--- src/Program.java
+++ src/Program.java
@@ -1,10 +1,15 @@
 public class Program {
-    private static GameStarter getGameStarter() {
-        return new ConsoleTest();
+    private static GameStarter getGameStarter(String[] args) {
+        for (var arg : args) {
+            if (arg.startsWith("--") && arg.equals("--test")) {
+                return new ConsoleTest();
+            }
+        }
+        throw new IllegalArgumentException("Invalid argument.");
     }
 
     public static void main(String[] args) {
-        var starter = Program.getGameStarter();
+        var starter = Program.getGameStarter(args);
         starter.startGame(args);
     }
 }
