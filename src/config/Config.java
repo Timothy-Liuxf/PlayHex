bf6961a3ba33d3983d37573436026a2a95ecea7b
Timothy Liu <liuxf19@163.com>
2023-07-04 17:57:39 +0800

    chore(deps): :wrench: update version to 0.2.0

--- src/config/Config.java
+++ src/config/Config.java
@@ -2,7 +2,7 @@ package config;
 
 public class Config {
     public static final String PRODUCT_NAME = "PlayHex";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.2.0";
     public static final String AUTHOR = "Timothy Liu Xuefeng";
     public static final String ABOUT_STRING = PRODUCT_NAME + " v" + VERSION
             + "\nCopyright (C) 2023 " + AUTHOR
